a1a89b865bf2dd5a590bda7017348069a823e9ef
Eliot Howes <ejhowes@gmail.com>
2019-05-26 11:01:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(string)
-  new_string = []
-  string.split("/ /")
-  string.each do |word|
+  new_string = string.split("/ /")
+  new_string.each do |word|
     new_string.push(word.reverse)
     return new_string
   end
