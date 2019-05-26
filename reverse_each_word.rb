8ce036da5f8261807aebaa5d22420eb5b03a52e5
Eliot Howes <ejhowes@gmail.com>
2019-05-26 10:41:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
+  new_string = []
   string.split(", ")
   string.each do |word|
-    word.reverse
+    new_string.push(word.reverse)
+    return new_string
   end
-  return string
 end
