870bfdd111717e211c9adfe956dc3b2a4c05f087
Eliot Howes <ejhowes@gmail.com>
2019-05-26 11:38:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,18 +9,8 @@ def reverse_each_word(sentence)
   sentence.split.collect {|word| word.reverse}.join(" ")
 end
 
-
-# string = string.split(" ")
-#    #i = 0
-#    #j = 0
-#    #newString = [" "]
-#    #while(i < string.length)
-#    #string.each do |string|
-#     #string = string.reverse()
-#     #newString[j] = string
-#       #i += 1
-#       #j += 1
-#    # end
-#      newString = string.collect{|reverse_string| reverse_string.reverse}
-#  #end
-#  return newString.join(' ')
+def reverse_each_word1(phrase)
+  phrase.split
+  phrase.collect {|word| word.reverse}
+  phrase.join(" ")
+end
